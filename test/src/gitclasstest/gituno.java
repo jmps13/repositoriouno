2611536eec156eecc7342f629b5a77d3f94614ad
Tarde <Tarde@Alumno13>
2013-02-07 18:40:03 +0100

    commit inicial

---
+++ test/src/gitclasstest/gituno.java
@@ -0,0 +1,9 @@
+package gitclasstest;
+
+public class gituno {
+	public static void main (String[] args){
+		System.out.println("prueba de git");
+		System.out.println("te cago en la bambas");
+	}
+
+}
