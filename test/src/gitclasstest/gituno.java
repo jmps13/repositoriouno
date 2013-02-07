b03a5a57f7858763b84289fa1fedb812e1da3c9b
Tarde <Tarde@Alumno13>
2013-02-07 18:40:55 +0100

    mejora la salida por pantalla

--- test/src/gitclasstest/gituno.java
+++ test/src/gitclasstest/gituno.java
@@ -4,6 +4,8 @@ public class gituno {
 	public static void main (String[] args){
 		System.out.println("prueba de git");
 		System.out.println("te cago en la bambas");
+		System.out.println("prueba de git");
+		System.out.println("te cago en la bambas");
 	}
 
 }
